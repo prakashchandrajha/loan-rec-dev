57424ce07ed124a9c3b37f97ccee0a0b19f53d10
Your Name <your.email@example.com>
2026-01-07 17:07:50 +0530

    1st commit

---
+++ src/main/java/com/npa_recovery/loan_rec_dev/LoanRecDevApplication.java
@@ -0,0 +1,15 @@
+package com.npa_recovery.loan_rec_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class LoanRecDevApplication {
+	TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	public static void main(String[] args) {
+		SpringApplication.run(LoanRecDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/npa_recovery/loan_rec_dev/LoanRecDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npa_recovery.loan_rec_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanRecDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
